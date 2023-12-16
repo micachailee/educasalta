5cb4202ac3599727dd559551861bb6e549afed05
micachailee <micachaile7@gmail.com>
2023-12-16 17:37:34 -0300

    Agregado de cards.js
    Se agrega para poder utilizar los filtros, falta el intercambio de información entre páginas

--- js/cards.js
+++ js/cards.js
@@ -1,10 +1,56 @@
 var cardsArticle = document.getElementById('main-content');
 
+var allData;
+
 fetch('../instituciones.json')
-.then(response => response.json())
-.then(data => {
-console.log('Datos del JSON:', data);
-data.forEach(function(item) {
+    .then(response => response.json())
+    .then(data => {
+        allData = data;
+        //FilterBy('nombre', 'salesiano');
+    })
+    .catch(error => console.error('Error al obtener el JSON:', error));
+
+function FilterBy(filter, value) {
+
+    if (allData) {
+        cardsArticle.innerHTML = '';
+        allData.forEach(function (item) {
+            if (filterMatches(item, filter, value)) {
+                createNewCard(item);
+            }
+        });
+    }
+}
+
+function filterMatches(item, filter, value) {
+
+    var filterFunctions = {
+        'nivel': function(item, value) {
+            return item.nivel.toLowerCase() === value.toLowerCase();
+        },
+        'nombre': function(item, value) {
+            return item.nombre.toLowerCase().includes(value.toLowerCase());
+        },
+        'sector': function(item, value) {
+            return item.sector.toLowerCase() === value.toLowerCase();
+        },
+        'modalidad':function(item, value) {
+            return item.modalidad.toLowerCase() === value.toLowerCase();
+        },
+        'zona': function(item, value) {
+            return item.zona.toLowerCase() === value.toLowerCase();
+        },
+    };
+
+    if (filterFunctions[filter]) {
+        return filterFunctions[filter](item, value);
+    }
+    return false;
+}
+
+
+function createNewCard(item){
+
     var cardSection = document.createElement('section');
     cardSection.classList.add('content-section');
 
@@ -15,11 +61,8 @@ data.forEach(function(item) {
     imgInst.src = item.imagen;
     imgInst.alt = 'Imagen de la institucion';
 
-    var titleInst = document.createElement('h2');
+    var titleInst = document.createElement('h3');
     titleInst.innerText = item.nombre;
-
-    var contentDetail = document.createElement('div');
-    contentDetail.classList.add('content-details');
 
     var readmButton = document.createElement('button');
     readmButton.classList.add('read-more');
@@ -29,10 +72,8 @@ data.forEach(function(item) {
     contentInfo.appendChild(titleInst);
 
     cardSection.appendChild(contentInfo);
-    cardSection.appendChild(contentDetail);
     cardSection.appendChild(readmButton);
 
     cardsArticle.appendChild(cardSection);
-});
-})
-.catch(error => console.error('Error al obtener el JSON:', error));
+}
+
